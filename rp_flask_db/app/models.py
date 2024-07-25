f8e14fc77bc0f8631a6be3caecf59a0d373f2386
XergSolana <124872583+XergSolana@users.noreply.github.com>
2024-07-25 17:11:49 +0200

    checkpoint
    works so far, not done yet.

--- rp_flask_db/app/models.py
+++
@@ -1,9 +0,0 @@
-from . import db
-
-class Gold(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    amount = db.Column(db.Integer, nullable=True)
-
-class Pickaxe(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    strength = db.Column(db.Integer, nullable=False)
